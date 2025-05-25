4f90224f49982d74a68385127f3eeb6ccedb4b85
Ibrahim Badr <himo7877@gmail.com>
2025-05-25 12:42:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Invalid src prop (https://picsum.photos/200) on `next/image`, hostname "picsum.photos" is not configured under images in your `next.config.js`
    > See more info: https://nextjs.org/docs/messages/next-image-unconfigured-host. Error source: src/components/courses/CourseCard.tsx (20:11) @ CourseCard
    >
    >   18 |       <CardHeader className="p-0">
    >   19 |         <Link href={`/courses/${course.id}`} className="block">
    > > 20 |           <Image
    >      |           ^
    >   21 |             src={course.imageUrl}
    >   22 |             alt={course.title}
    >   23 |             width={600}
    >
    > Call Stack
    > 8
    >
    > Show 5 ignore-listed frame(s)
    > CourseCard
    > src/components/courses/CourseCard.tsx (20:11)
    > <anonymous>
    > src/app/page.tsx (53:15)
    > HomePage
    > src/app/page.tsx (52:22)

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.vectorstock.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
